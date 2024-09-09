276c781cad7e984c399ad10d45e1f30d2528f202
li1553770945 <1553770945@qq.com>
2024-09-09 14:20:40 +0800

    添加签证和注册信息

--- src/pages/Registration.vue
+++ src/pages/Registration.vue
@@ -1,11 +1,78 @@
 <template>
-    <h2>Registration</h2>
-    under construction......
+    <h2>Registration Fee Schedule</h2>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th></th>
+            <th>Registration fee (early bird by 30 Sep)</th>
+            <th>Registration fee (after 30 Sep)</th>
+        </tr>
+        <tr>
+            <td>Author registration</td>
+            <td>AUD$1200 (CNY¥6000)</td>
+            <td>AUD$1400 (CNY¥7000)</td>
+        </tr>
+        <tr>
+            <td>Second and next author or non-author registration</td>
+            <td>AUD$1000 (CNY¥5000)</td>
+            <td>AUD$1200 (CNY¥6000)</td>
+        </tr>
+        <tr>
+            <td>Extra Page (max. 2 pages)</td>
+            <td>AUD$150 (CNY¥750)</td>
+            <td>AUD$150 (CNY¥750)</td>
+        </tr>
+        <tr>
+            <td>Extra Banquet Ticket</td>
+            <td>AUD$250 (CNY¥1200)</td>
+            <td>AUD$250 (CNY¥1200)</td>
+        </tr>
+        <tr>
+            <td>Extra Excursion Ticket</td>
+            <td>AUD$250 (CNY¥1200)</td>
+            <td>AUD$250 (CNY¥1200)</td>
+        </tr>
+    </table>
+    <h2>Registration Policy</h2>
+    AT LEAST ONE author registration for each accepted paper should be made in time for inclusion of the paper in the
+    IEEE proceedings.
+    <h2>Registration Steps</h2>
+
+    <ol>
+        <li>
+            Refer to the table above and select the right category/amount that applies to your case, such as:
+            <ul>
+                <li>Author registration</li>
+                <li>Second author registration</li>
+                <li>Extra page</li>
+                <li>Other applicable categories</li>
+            </ul>
+        </li>
+        <li>
+            Log in to the following page:
+            <a href="https://cbd24.dailyEliteEvents.com.au" target="_blank">CBD'24 Conference Registration</a>.
+            Please refer to the email communication from the CBD'24 conference Organizing Committee for your login
+            details.
+        </li>
+        <li>
+            Make your payment following the instructions on the web page and quote the participants name and paper ID you are registering in the transaction note for identification.
+        </li>
+        <li>
+            Send a confirmation email to:
+            <a href="mailto:CBD24@DailyEliteEvents.com.au">CBD24@DailyEliteEvents.com.au</a>.
+            The confirmation email must include:
+            <ul>
+                <li>The participant's name</li>
+                <li>The registration category</li>
+                <li>The ID and title of your paper</li>
+            </ul>
+        </li>
+    </ol>
+
 
 </template>
 
 <style scoped>
-h2{
+h2 {
     color: #005f98;
 }
 </style>
--- src/pages/Visa.vue
+++ src/pages/Visa.vue
@@ -1,11 +1,18 @@
 <template>
-    <h2>Visa</h2>
-    under construction......
+    <h2>Visa Letter Requests</h2>
+    If you need a letter of invitation for visa purposes, please send the following information to <a href="mailto:dingding-1@seu.edu.cn">dingding-1@seu.edu.cn</a> before September 13, 2024.
+    <ul>
+    <li>Your full name</li>
+    <li>Your affiliation</li>
+    <li>Your title (e.g., Prof./Dr./Mr./Ms.)</li>
+    <li>You email address</li>
+    <li>your paper’s ID and title</li>
+    </ul>
 
 </template>
 
 <style scoped>
-h2{
+h2 {
     color: #005f98;
 }
 </style>
